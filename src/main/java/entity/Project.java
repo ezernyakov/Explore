3ecbba964a4cc7f4daa3c86a2f65172753c3bd32
ezernyakov <e.zernyakov@crystals.ru>
2020-10-25 18:34:02 +0300

    add project / edit project / remove project

--- src/main/java/entity/Project.java
+++ src/main/java/entity/Project.java
@@ -1,17 +1,30 @@
 package entity;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Project {
 
-    private int id;
+    private String id;
     private String name;
     private String description;
+    private static AtomicInteger count;
 
     public Project(String name, String description) {
+        if(count == null) {
+            count = new AtomicInteger(0);
+        }
+        this.id = "PR-" + count.getAndIncrement();
         this.name = name;
         this.description = description;
     }
 
-    public int getId() {
+    public Project(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
         return id;
     }
 
--- src/main/java/web/controllers/ProjectController.java
+++ src/main/java/web/controllers/ProjectController.java
@@ -2,10 +2,7 @@ package web.controllers;
 
 import entity.Project;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,33 +13,52 @@ import java.util.Map;
 public class ProjectController {
 
     private List<Project> projects = new ArrayList<>();
+    private Project currentProject;
 
     @GetMapping("/add")
-    public String addProject()  {
+    public String openAddProjectPage() {
         return "project/add";
     }
 
     @PostMapping("/add")
-    public String saveProject(@RequestParam String name, @RequestParam String description) {
+    public String createProject(@RequestParam String name, @RequestParam String description) {
         Project project = new Project(name, description);
         projects.add(project);
         return "main";
     }
 
-    @GetMapping("/edit")
-    public String editProject(Map<String, Object> model)  {
-
+    @PostMapping("/edit")
+    public String openEditProjectPage(@RequestParam String id, @RequestParam String name, @RequestParam String description,
+                                      Map<String, Object> model) {
+        currentProject = new Project(id, name, description);
+        model.put("project", currentProject);
         return "project/edit";
     }
 
-    @GetMapping("/remove")
-    public String removeProject(Map<String, Object> model)  {
+    @PostMapping(value="/list", params={"id", "name", "description"})
+    public String saveChanges(@RequestParam String id, @RequestParam String name, @RequestParam String description,
+                              Map<String, Object> model) {
+        for (Project project : projects) {
+            if (project.getId().equals(id)) {
+                project.setName(name);
+                project.setDescription(description);
+            }
+        }
 
-        return "project/remove";
+        model.put("projects", projects);
+        return "project/list";
+    }
+
+    @PostMapping(value="/list", params = "id")
+    public String removeProject(@RequestParam String id, Map<String, Object> model) {
+        projects.removeIf(project -> project.getId().equals(id) );
+
+        model.put("projects", projects);
+        return "project/list";
     }
 
     @GetMapping("/list")
-    public String getListProject(Map<String, Object> model)  {
+    public String openListProjectPage(Map<String, Object> model) {
         model.put("projects", projects);
         return "project/list";
     }
